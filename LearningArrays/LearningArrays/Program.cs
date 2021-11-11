7a95363600f2c487f563a1558f16e89cc5ae2dc9
MonikaS <moni.staniulyte@gmail.com>
2021-11-11 20:18:01 +0200

    Create C# project. Add array loop logic.

---
+++ LearningArrays/LearningArrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stringArray = new string[] { "a", "b", "c", "d", "e"};
+
+            for (var i = 0; i < stringArray.Length; i++)
+            {
+                Console.WriteLine(stringArray[i]);
+            }
+
+            Console.WriteLine();
+
+            var intArray = Enumerable.Range(0, 20).ToArray();
+
+            for (var i = intArray.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(intArray[i]);
+            }
+
+            // to prevent console from closing
+            Console.ReadLine();
+        }
+    }
+}
